ee7f5b784b5338aa748ee0a7ac3e151bf08f32cf
Kannan Manickam <me@arangamani.net>
2013-07-20 14:48:46 -0700

    code cleanup

--- src/main/java/net/arangamani/jenkins/gempublisher/RubygemsApi.java
+++ src/main/java/net/arangamani/jenkins/gempublisher/RubygemsApi.java
@@ -6,7 +6,6 @@ import java.io.InputStream;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
